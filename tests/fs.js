600ffd429b742435105a6c7221fff8a790c8fbbc
Minifyre <minifyre@gmail.com>
2018-12-12 17:03:22 -0600

    +writeDir test

--- tests/fs.js
+++ tests/fs.js
@@ -5,19 +5,17 @@ import run from '../../cherub/index.js'
 const
 dirTmp=['read.txt'],
 readTxt='some text',
-mkOpts=function()
-{
-	return {
-		cleanup:async function()
-		{
-			return
-		}
-	}
-},
+emptyDir=src=>file.readDir(src).then(x=>Array.isArray(x)&&!x.length),
+curry=(fn,...xs)=>(...ys)=>fn(...xs,...ys),
 tests=
 [
 	[()=>file.readDir('tmp'),dirTmp,'readDir'],
-	[()=>file.readFile('tmp/read.txt'),readTxt,'readFile']
+	[()=>file.readFile('tmp/read.txt'),readTxt,'readFile'],
+	[
+		()=>file.writeDir('tmp/a'),
+		()=>emptyDir('tmp/a/'),
+		{name:'writeDir',cleanup:curry(file.deleteDir,'tmp/a/')}
+	]
 ],
 //@todo re-enable shuffle
 opts={now:()=>performance.now(),parallel:false,shuffle:false}
